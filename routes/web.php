832bf680d35abe04788b655ed4c0e6d31e689900
unknown <shantkevv@gmail.com>
2021-05-04 05:27:08 -0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,3 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
-Route::post('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
