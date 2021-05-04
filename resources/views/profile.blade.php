787f56672313a2e48210670cd4eff5e7c4c2d477
unknown <shantkevv@gmail.com>
2021-05-04 05:29:57 -0700

    first commit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -14,6 +14,14 @@
                             @csrf
 
                             <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Old E-mail Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('New E-mail Address') }}</label>
 
                                 <div class="col-md-6">
