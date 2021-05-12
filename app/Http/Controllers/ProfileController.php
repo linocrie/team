a94f6d776be0943d82c4e41c26545b3f1e89d3be
unknown <shantkevv@gmail.com>
2021-05-12 10:59:59 +0400

    Modified

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Avatar;
 use App\Models\Profession;
-use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Detail;
 use Illuminate\Support\Facades\Auth;
