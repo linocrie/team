a51694925864959d323c1f0cd9d3690318267e3c
unknown <shantkevv@gmail.com>
2021-06-03 19:11:57 +0400

    Filter and Search Professions

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('is_admin');
+//        $this->middleware('is_admin');
     }
 
     public function  index() {
