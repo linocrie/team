d9f05253a890d462b888875a0c7e8a89675f21fc
linocrie <zaqaryanliana14@gmail.com>
2021-06-03 19:01:36 +0400

    professions page

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,4 @@ require('./bootstrap');
 require('./gallery_filter_search');
 require('./multiselect');
 require('./disablebutton');
-<<<<<<< HEAD
 require('./users_filter_search');
-=======
-
->>>>>>> 1b9427a6196db21c44e528868ce92a81e99f2e66
--- resources/js/multiselect.js
+++ resources/js/multiselect.js
@@ -1,10 +1,3 @@
 $(function () {
     $('#multiSelect').multipleSelect();
 });
-<<<<<<< HEAD
-=======
-
-$(function () {
-    $('#filters').multipleSelect();
-});
->>>>>>> 1b9427a6196db21c44e528868ce92a81e99f2e66
