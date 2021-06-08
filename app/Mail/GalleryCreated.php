b35e96022f52957b9b7fb9aaa7b60b62c222e365
Khachik <khachik.grigoryan1997@gmail.com>
2021-06-08 12:47:42 +0400

    galleryMailSuccess

---
+++ app/Mail/GalleryCreated.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Gallery;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\Request;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class GalleryCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct($gallery)
+    {
+         $this->gallery = $gallery;
+
+    }
+
+
+    public function build()
+    {
+
+        return $this->view('emails.gallery')
+            ->with('gallery', $this->gallery->load(['images']));
+
+    }
+}
---
+++ resources/views/emails/gallery.blade.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <title></title>
+    <!--[if (mso 16)]>
+    <style type="text/css">
+        a {text-decoration: none;}
+    </style>
+    <![endif]-->
+    <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+    <!--[if gte mso 9]>
+    <xml>
+        <o:OfficeDocumentSettings>
+            <o:AllowPNG></o:AllowPNG>
+            <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+    </xml>
+    <![endif]-->
+</head>
+
+<body>
+<div class="es-wrapper-color">
+    <!--[if gte mso 9]>
+    <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+        <v:fill type="tile" color="#f1f1f1"></v:fill>
+    </v:background>
+    <![endif]-->
+    <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0">
+        <tbody>
+        <tr>
+            <td class="esd-email-paddings" valign="top">
+                <table cellpadding="0" cellspacing="0" class="es-content esd-header-popover" align="center">
+                    <tbody>
+                    <tr>
+                        <td class="esd-stripe" esd-custom-block-id="7958" align="center">
+                            <table class="es-content-body" style="background-color: transparent;" width="600" cellspacing="0" cellpadding="0" align="center">
+                                <tbody>
+                                <tr>
+                                    <td class="esd-structure es-p15t es-p15b es-p10r es-p10l" align="left">
+                                        <!--[if mso]><table width="580" cellpadding="0" cellspacing="0"><tr><td width="282" valign="top"><![endif]-->
+                                        <table class="es-left" cellspacing="0" cellpadding="0" align="left">
+                                            <tbody>
+                                            <tr>
+                                                <td class="esd-container-frame" width="282" align="left">
+                                                    <table width="100%" cellspacing="0" cellpadding="0">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td class="es-infoblock esd-block-text es-m-txt-c" align="left">
+                                                                <p style="font-family: arial, helvetica\ neue, helvetica, sans-serif;">Success<br></p>
+                                                            </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                        <!--[if mso]></td><td width="20"></td><td width="278" valign="top"><![endif]-->
+
+                                        <!--[if mso]></td></tr></table><![endif]-->
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+               
+                <table class="es-content" cellspacing="0" cellpadding="0" align="center">
+                    <tbody>
+                    <tr>
+                        <td class="esd-stripe" esd-custom-block-id="7388" align="center">
+                            <table class="es-content-body" style="background-color: #333333;" width="600" cellspacing="0" cellpadding="0" bgcolor="#333333" align="center">
+                                <tbody>
+                                <tr>
+                                    <td class="esd-structure esd-checked es-p40t es-p40b es-p40r es-p40l" style="background-image: url('https://tlr.stripocdn.email/content/guids/CABINET_85e4431b39e3c4492fca561009cef9b5/images/93491522393929597.png'); background-repeat: no-repeat;" align="left">
+                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                            <tbody>
+                                            <tr>
+                                                <td class="esd-container-frame" width="520" valign="top" align="center">
+                                                    <table width="100%" cellspacing="0" cellpadding="0">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td align="center" class="esd-block-text es-p40t es-p10b">
+                                                                <h1 style="color: #ffffff;">Gallery {{$gallery->title}} was successfully created</h1>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td esdev-links-color="#757575" class="esd-block-text es-p10t es-p20b es-p30r es-p30l" align="center">
+                                                                <p style="color: whitesmoke;">We are happy for your visit  </p>
+                                                                <p style="color: whitesmoke;">You have {{$gallery->images->count()}} images in {{$gallery->title}} gallery</p>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="esd-block-button es-p10t es-p20b" align="center"><span class="es-button-border" style="border-width: 0px; border-style: solid; background: none 0% 0% repeat scroll #26a4d3; border-color: #26a4d3;"><a href="http://127.0.0.1:8000/" class="es-button" target="_blank" style="background: #26a4d3 none repeat scroll 0% 0%; border-color: #26a4d3;">Go Instagram Clone</a></span></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <table cellpadding="0" cellspacing="0" class="es-footer" align="center">
+                    <tbody>
+                    <tr>
+                        <td class="esd-stripe" esd-custom-block-id="7394" align="center">
+                            <table class="es-footer-body" style="background-color: #ffffff;" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+                                <tbody>
+                                <tr>
+                                    <td class="esd-structure es-p40t es-p40b es-p40r es-p40l" align="left">
+                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                            <tbody>
+                                            <tr>
+                                                <td class="esd-container-frame" width="520" valign="top" align="center">
+                                                    <table width="100%" cellspacing="0" cellpadding="0">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td class="esd-block-text es-p10b" align="center">
+                                                                <p>Instagram Clone</p>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="esd-block-text es-p10b" align="center">
+                                                                <p>This email was sent to you from Company Email Address</p>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center" class="esd-block-text es-p10b">
+                                                                <p><a target="_blank" href="https://viewstripo.email/">Preferences</a> | <a target="_blank" href="https://viewstripo.email/">Browser</a> | <a target="_blank" href="https://viewstripo.email/">Forward</a> | <a target="_blank" class="unsubscribe">Unsubscribe</a></p>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="esd-block-text" align="center">
+                                                                <p>Copyright © 2015-2018 <strong>Company Name</strong>, All Rights Reserved.</p>
+                                                            </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table class="esd-footer-popover es-content" cellspacing="0" cellpadding="0" align="center">
+                    <tbody>
+                    <tr>
+                        <td class="esd-stripe" align="center">
+                            <table class="es-content-body" style="background-color: transparent;" width="600" cellspacing="0" cellpadding="0" align="center">
+                                <tbody>
+                                <tr>
+                                    <td class="esd-structure es-p30t es-p30b es-p20r es-p20l" align="left">
+                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                            <tbody>
+                                            <tr>
+                                                <td class="esd-container-frame" width="560" valign="top" align="center">
+                                                    <table width="100%" cellspacing="0" cellpadding="0">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td class="esd-block-image es-infoblock made_with" align="center" style="font-size:0"><a target="_blank" href="https://viewstripo.email/?utm_source=templates&utm_medium=email&utm_campaign=information&utm_content=trigger_newsletter4"><img src="https://tlr.stripocdn.email/content/guids/CABINET_9df86e5b6c53dd0319931e2447ed854b/images/64951510234941531.png" alt width="125"></a></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+
+</html>
